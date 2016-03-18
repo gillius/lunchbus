c75aa02e50b8c09e44eff25f0679023c0011fa0d
Jason Winnebeck <gillius@gillius.org>
2016-03-17 23:39:07 -0400

    Make everything look a little bit nicer with some CSS and Bootstrap

--- src/main/resources/static/TagsComponent.js
+++ src/main/resources/static/TagsComponent.js
@@ -13,12 +13,16 @@ angular.module('webapp').component('tags', {
 			mp.send(this.route + '/removeTag', {name: this.parentName, tag: tagName});
 		}
 	},
-	template: "<span ng-repeat='tag in $ctrl.tags track by $index'>" +
+	template: "<span class='tag label label-info' ng-repeat='tag in $ctrl.tags track by $index'>" +
 	"{{tag}} " +
-	"<a href='#' ng-click='$ctrl.removeTag(tag)'>(&times;)</a>, " +
+	"<a class='remove' href='#' ng-click='$ctrl.removeTag(tag)'>&times;</a>" +
 	"</span>" +
-	"<form style='display: inline' name='tagsForm' ng-submit='$ctrl.addTag()'>" +
-	"<input type='text' ng-model='$ctrl.tagName' ng-required='true'>" +
-	"<button type='submit' ng-disabled='!tagsForm.$valid'>+</button>" +
+	"<form class='form-inline' style='display: inline' name='tagsForm' ng-submit='$ctrl.addTag()'>" +
+	"<div class='input-group'>" +
+	"<input class='form-control input-sm' type='text' ng-model='$ctrl.tagName' ng-required='true'> " +
+	"<span class='input-group-btn'>" +
+	"<button class='btn btn-default btn-sm' type='submit' ng-disabled='!tagsForm.$valid'>+</button>" +
+	"</span>" +
+	"</div>" +
 	"</form>"
 });
